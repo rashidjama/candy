4e0783b54cda34891339b459a35083b7e4208d8c
Rashid Jama <rashidjama5@gmail.com>
2020-02-09 17:22:58 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,27 @@
+function takeANumber(deliLine, customerName) {
+  deliLine.push(customerName);
+  return `Welcome, ${customerName}. You are number ${deliLine.length} in line.`;
+}
+
+function nowServing(katzDeli) {
+  if(katzDeli.length === 0) {
+    return 'There is nobody waiting to be served!'
+  } else {
+    return `Currently serving ${katzDeli.shift()}.`
+  }
+}
+
+function currentLine(katzDeli) {
+
+  if(katzDeli.length === 0) 
+    return 'The line is currently empty.';
+
+  let line = "";
+  for (let i = 0; i < katzDeli.length; i++) {
+    line += i + 1 + ". " + katzDeli[i] + ", ";
+  }
+  
+    line = line.substring(0, line.length - 2);
+  
+  return `The line is currently: ${line}`;
+}
